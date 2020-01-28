ecbf21aea80f0ed17516b9b0c4cbb7172d2f5a78
pahoffart25 <pahoffart25@gmail.com>
2020-01-27 22:32:20 -0600

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
-end
+def my_select(collection) 
+    counter = 0 
+    result = []
+    while counter < collection.length
+      if yield(collection[counter]) == true
+        result.push(collection[counter])
+      end
+      counter+=1
+    end
+    result
+  end
